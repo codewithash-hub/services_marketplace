b3634928c7da51096c0675b8099f29ad6fed0e3b
unknown <402308182@my.richfield.ac.za>
2025-08-08 23:11:01 +0200

    Creating a model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(models.Model):
+    class Gender(models.TextChoices):
+        MALE = 'M', 'Male'
+        FEMALE = 'F', "Female"
+
+    name = models.CharField(max_length=255)
+    gender = models.CharField(
+        max_length=2,
+        choices=Gender,
+        default=Gender.MALE
+    )
+    is_provider = models.BooleanField(default=False) # This is to determine if the user is a provider or a client
+    phone_number = models.CharField(max_length=20, blank=True)
+    town = models.CharField(max_length=255, blank=True)
+    suburb = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.name
